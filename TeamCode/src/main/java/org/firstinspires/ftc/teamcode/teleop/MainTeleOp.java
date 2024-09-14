8bd76d69636a507812a697686ef35371f33c357b
justin <justin.jc.wang@gmail.com>
2024-09-14 10:57:37 -0400

    added ftclib and gamepads to code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class MainTeleOp extends LinearOpMode {
+    private GamepadEx gp1, gp2;
 
     public void runOpMode() {
+        gp1 = new GamepadEx(gamepad1);
+        gp2 = new GamepadEx(gamepad2);
 
     }
 }
