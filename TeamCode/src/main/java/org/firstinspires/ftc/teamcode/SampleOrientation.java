1b9f2b1d8e046e8420df0fa40b90a6e26ecf8e11
justin <justin.jc.wang@gmail.com>
2024-10-12 14:32:34 -0400

    LETS GO IT WORKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOrientation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOrientation.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.util.Size;
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
@@ -10,8 +11,6 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
-import org.opencv.core.Point;
-import org.opencv.core.CvType;
 import org.opencv.imgproc.Imgproc;
 
 
@@ -19,20 +18,23 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class SampleOrientation implements VisionProcessor {
-
+    private Mat Frame = new Mat();
     private Mat HsvMat = new Mat();
     private Mat RedMask = new Mat();
     private Mat BlueMask = new Mat();
     private Mat YellowMask = new Mat();
 
     private List<MatOfPoint> RedContours = new ArrayList<>();
-    private java.util.List<MatOfPoint> BlueContours = new ArrayList<>();
-    private java.util.List<MatOfPoint> YellowContours = new ArrayList<>();
+    private List<MatOfPoint> BlueContours = new ArrayList<>();
+    private List<MatOfPoint> YellowContours = new ArrayList<>();
 
-    private final Scalar redHSV = new Scalar(0,0,0);
-    private final Scalar blueHSV = new Scalar(0,0,0);
-    private final Scalar yellowHSV = new Scalar(0,0,0);
-    private final Scalar range = new Scalar(0,0,0);
+    private final Scalar redHSV = new Scalar(0,175,127);
+    private final Scalar blueHSV = new Scalar(231/2,125,127);
+    private final Scalar yellowHSV = new Scalar(49.5/2,175,127);
+
+    private final Scalar redRange = new Scalar(5,79,127);
+    private final Scalar blueRange = new Scalar(10,80,127);
+    private final Scalar yellowRange = new Scalar(5,79,127);
 
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
@@ -41,9 +43,11 @@ public class SampleOrientation implements VisionProcessor {
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
         Imgproc.cvtColor(frame, HsvMat, Imgproc.COLOR_RGB2HSV);
-        Core.inRange(HsvMat,range(redHSV, range, false),range(redHSV, range, true),RedMask);
-        Core.inRange(HsvMat,range(blueHSV, range, false),range(redHSV, range, true),BlueMask);
-        Core.inRange(HsvMat,range(yellowHSV, range, false),range(redHSV, range, true),YellowMask);
+        Frame = frame;
+
+        Core.inRange(HsvMat,range(redHSV, redRange, false),range(redHSV, redRange, true),RedMask);
+        Core.inRange(HsvMat,range(blueHSV, blueRange, false),range(blueHSV, blueRange, true),BlueMask);
+        Core.inRange(HsvMat,range(yellowHSV, yellowRange, false),range(yellowHSV, yellowRange, true),YellowMask);
 
         Imgproc.findContours(RedMask, RedContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         Imgproc.findContours(BlueMask, BlueContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
@@ -53,12 +57,13 @@ public class SampleOrientation implements VisionProcessor {
 
     @Override
     public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-        Mat matCanvas = new Mat(onscreenHeight, onscreenWidth, CvType.CV_8UC3);
-        matCanvas.setTo(new Scalar(0, 0, 0));
+        Mat matCanvas = Frame;
 
-        Imgproc.drawContours(matCanvas, RedContours, -1, new Scalar(0,0,255), 5);
-        Imgproc.drawContours(matCanvas, BlueContours, -1, new Scalar(255,0,0), 5);
-        Imgproc.drawContours(matCanvas, YellowContours, -1, new Scalar(0,240,240), 5);
+        Imgproc.drawContours(matCanvas, RedContours, -1, new Scalar(255,0,0), 5);
+        Imgproc.drawContours(matCanvas, BlueContours, -1, new Scalar(0,0,255), 5);
+        Imgproc.drawContours(matCanvas, YellowContours, -1, new Scalar(240,240,0), 5);
+
+        RedContours.clear(); BlueContours.clear(); YellowContours.clear();
 
         // Convert Mat to Bitmap
         Bitmap bitmap = Bitmap.createBitmap(matCanvas.cols(), matCanvas.rows(), Bitmap.Config.ARGB_8888);
@@ -73,16 +78,16 @@ public class SampleOrientation implements VisionProcessor {
         Scalar ret = new Scalar(0,0,0);
         if (retUpper) {
             ret = new Scalar (
-                (input.val[0] + range.val[0])%255,
-                (input.val[1] + range.val[1])%255,
-                (input.val[2] + range.val[2])%255
-                );
+                    Math.max((input.val[0] + range.val[0])%255, 0.0),
+                    Math.max((input.val[1] + range.val[1])%255, 0.0),
+                    Math.max((input.val[2] + range.val[2])%255, 0.0)
+            );
         } else {
             ret = new Scalar (
-                (input.val[0] - range.val[0])%255,
-                (input.val[1] - range.val[1])%255,
-                (input.val[2] - range.val[2])%255
-                );
+                    Math.max((input.val[0] - range.val[0])%255, 0.0),
+                    Math.max((input.val[1] - range.val[1])%255, 0.0),
+                    Math.max((input.val[2] - range.val[2])%255, 0.0)
+            );
         }
         return ret;
     }
