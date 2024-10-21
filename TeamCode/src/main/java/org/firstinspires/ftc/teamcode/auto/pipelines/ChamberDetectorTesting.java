552651973bf7af802de22a0899b5eded2b0aeeb7
jeremiahchou <Booseffph@gmail.com>
2024-10-21 16:40:48 -0400

    started chamber detector testing 🐒

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pipelines/ChamberDetectorTesting.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.auto.pipelines;
+
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Core;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Scalar;
+public class ChamberDetectorTesting {
+    public static int camWidth = 1280;
+    public static int camHeight = 720;
+}
