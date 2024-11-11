889ed1f145e7db4a3e4de21fd6c3a615557ea25b
Vignesha Jayakumar <jayakvv@gmail.com>
2024-11-10 21:24:11 -0500

    removed jerry test actions code (raahil's orders 🙄)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/TestAutonomous.java
@@ -29,117 +29,7 @@ import org.firstinspires.ftc.teamcode.teleop.subsystems.Bot;
 @Autonomous(name = "TEST_AUTONOMOUS", group = "Autonomous")
 public class TestAutonomous extends LinearOpMode {
     private Bot bot;
-    public class Arm {
-        private ServoEx armLeft, armRight;
 
-        public Arm(HardwareMap hardwareMap) {
-            armLeft = hardwareMap.get(ServoEx.class, "armLeft");
-            armRight = hardwareMap.get(ServoEx.class, "armRight");
-        }
-
-        public class storage implements Action {
-
-            @Override
-            public boolean run(@NonNull TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-        public Action storage() {
-            return new storage();
-        }
-
-        public class wallIntake implements Action {
-
-            @Override
-            public boolean run(@NonNull TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-        public Action wallIntake() {
-            return new wallIntake();
-        }
-
-        public class frontPickup implements Action {
-
-            @Override
-            public boolean run(@NonNull TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-        public Action frontPickup() {
-            return new frontPickup();
-        }
-
-        public class bucket implements Action {
-
-            @Override
-            public boolean run(@NonNull TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-        public Action bucket() {
-            return new bucket();
-        }
-
-        public class chamber implements Action {
-
-            @Override
-            public boolean run(@NonNull TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-        public Action chamber() {
-            return new chamber();
-        }
-
-    }
-    public class Gripper {
-        private ServoEx servo;
-
-        public Gripper(HardwareMap hardwareMap) {
-            servo = hardwareMap.get(ServoEx.class, "servo");
-        }
-
-        public class open implements Action {
-
-            @Override
-            public boolean run(TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-        public Action open() {
-            return new open();
-        }
-
-        public class close implements Action {
-
-            @Override
-            public boolean run(TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-        public Action close() {
-            return new close();
-        }
-    }
-    public class Pivot {
-        private DcMotorEx motor;
-
-        public Pivot(HardwareMap hardwareMap) {
-            motor = hardwareMap.get(DcMotorEx.class, "motor");
-        }
-
-        public class storage implements Action {
-
-            @Override
-            public boolean run(TelemetryPacket packet) {
-                return false; // placeholder
-            }
-        }
-    }
-    public class Slides {
-
-    }
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -150,6 +40,8 @@ public class TestAutonomous extends LinearOpMode {
         bot = Bot.getInstance(this);
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        
 
         // new Pose2d(10, 58, Math.toRadians(-90))
         Action blueSmall = drive.actionBuilder(drive.pose)
